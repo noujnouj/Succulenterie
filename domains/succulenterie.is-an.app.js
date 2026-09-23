2732b3ebba2bd9fc92a0258bc3b93a6891fcd05b
noujnouj <lateamroketnesenvolepas@gmail.com>
2026-09-23 22:25:00 +0100

    Add subdomain configuration for Succulenterie

---
+++ domains/succulenterie.is-an.app.js
@@ -0,0 +1,11 @@
+addSubDomain({
+  description: 'Serveur Minecraft prive - Succulenterie',
+  domain: 'is-an.app',
+  subdomain: 'succulenterie',
+  owner: {
+    email: 'ton-email@example.com', // remplace par ton vrai email
+  },
+  record: {
+    A: ['45.136.214.234'],
+  },
+})
