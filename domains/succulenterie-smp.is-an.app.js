68564716d3f9d74c8bdc65e4b39ea9e26f908808
noujnouj <lateamroketnesenvolepas@gmail.com>
2026-09-23 22:29:31 +0100

    Add subdomain for SMP minecraft succulent

---
+++ domains/succulenterie-smp.is-an.app.js
@@ -0,0 +1,11 @@
+addSubDomain({
+  description: 'SMP minecraft succulent - Succulenterie',
+  domain: 'is-an.app',
+  subdomain: 'succulenterie-smp',
+  owner: {
+    email: 'lateamroketnesenvolepas@gmail.com',
+  },
+  record: {
+    A: ['45.136.214.234'],
+  },
+})
